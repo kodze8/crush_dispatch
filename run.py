ff966d54b045f1adf5c3ecf6899d1e68425b12f5
kodze8 <mariamsaiqodze@gmail.com>
2024-09-02 13:22:23 +0200

    last commit

--- run.py
+++ run.py
@@ -4,17 +4,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5004)
 
 
-
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-#
-# if __name__ == '__main__':
-#     app.run()
